cac923ebc53c81caf382f55698e25093fc9c55be
Jaaassim <altemailforjjmoes@gmail.com>
2024-11-28 23:24:34 +0300

    Add files via upload

---
+++ color.js
@@ -0,0 +1,55 @@
+var redx = document.getElementById("redid"); 
+
+function redfunc() { 
+  redx.play(); 
+} 
+
+
+var orangex = document.getElementById("orangeid"); 
+
+function orangefunc() { 
+  orangex.play(); 
+} 
+
+
+var yellowx = document.getElementById("yellowid"); 
+
+function yellowfunc() { 
+  yellowx.play(); 
+} 
+
+
+
+var greenx = document.getElementById("greenid"); 
+
+function greenfunc() { 
+  greenx.play(); 
+} 
+
+
+var bluex = document.getElementById("blueid"); 
+
+function bluefunc() { 
+  bluex.play(); 
+} 
+
+
+var purplex = document.getElementById("purpleid"); 
+
+function purplefunc() { 
+  purplex.play(); 
+} 
+
+
+var pinkx = document.getElementById("pinkid"); 
+
+function pinkfunc() { 
+  pinkx.play(); 
+} 
+
+
+var brownx = document.getElementById("brownid"); 
+
+function brownfunc() { 
+  brownx.play(); 
+} 
